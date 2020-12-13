a18cb5b3ff86e769bf35c549fbd038789657515e
Thiago Mota <thiago_mota2017@hotmail.com>
2020-12-13 13:43:48 -0300

    Configs to deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,15 +14,12 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
+  target: 'static',
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     './assets/fonts/Yanone.css'
   ],
 
-  options: {
-    dist: 'dist'
-  },
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
   ],
